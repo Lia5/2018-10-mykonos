30d621affcde77d3df95dc662dae975142b71a7c
Елена Портная <e.portnaya@largespace.ru>
2018-10-28 17:23:36 +0200

    header tabs

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,29 @@
 
 document.addEventListener('DOMContentLoaded', function(){
 
-
+//tabs
+	// store tabs variable
+	var myTabs = document.querySelectorAll("ul.header__tabs > li");
+    function myTabClicks(tabClickEvent) {
+		for (var i = 0; i < myTabs.length; i++) {
+			myTabs[i].classList.remove("active");
+		}
+		var clickedTab = tabClickEvent.currentTarget;
+		clickedTab.classList.add("active");
+		tabClickEvent.preventDefault();
+		var myContentPanes = document.querySelectorAll(".tab-pane");
+		for (i = 0; i < myContentPanes.length; i++) {
+			myContentPanes[i].classList.remove("active");
+		}
+        var anchorReference = tabClickEvent.target;
+        console.log(anchorReference);
+        var activePaneId = anchorReference.getAttribute("href");
+        console.log(activePaneId);
+        var activePane = document.querySelector(activePaneId);
+        console.log(activePaneId);
+		activePane.classList.add("active");
+	}
+	for (i = 0; i < myTabs.length; i++) {
+		myTabs[i].addEventListener("click", myTabClicks)
+	}
 });
